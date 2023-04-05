08b0d7a6bae4c23cebae49420a358f42fe67e0da
halex22 <73904950+halex22@users.noreply.github.com>
2023-04-05 11:24:42 +0200

    add forms.py

---
+++ forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms.validators import DataRequired, URL, Email
+from flask_ckeditor import CKEditorField
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+class CreateUser(FlaskForm):
+    name = StringField("User's name", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    passord = PasswordField("User's Password", validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+
+class LoginUser(FlaskForm):
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("User's Password", validators=[DataRequired()])
+    submit = SubmitField("Log in")
+
+
+class CreateComment(FlaskForm):
+    body = CKEditorField("Add comment", validators=[DataRequired()])
+    submit = SubmitField("Submit comment")
